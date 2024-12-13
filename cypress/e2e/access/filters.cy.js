0ce586d23d9a3553c1006fb9a67579bd18836121
devVargas <alissonvargas97@gmail.com>
2024-12-13 15:42:06 -0300

    Filtro Data

--- cypress/e2e/access/filters.cy.js
+++ cypress/e2e/access/filters.cy.js
@@ -1,10 +1,12 @@
-describe("Valida os filtros do menu Acessos", () => {
+import dayjs from "dayjs";  // eslint-disable-line no-unused-vars
+
+describe("Valida os filtros do menu 'Acessos'", () => {
   beforeEach(() =>{
     cy
       .login();
   });
 
-  it("Faz a validação do do botão 'Acessos ativos' dentro do menu 'Iniciar'", 
+  it("Filtro Acesso", 
     () => {
       cy
         .licenca();
@@ -12,10 +14,35 @@ describe("Valida os filtros do menu Acessos", () => {
         .contains("li", "Acessos")
         .click()
         .get("input[placeholder='Acesso']")
-        .type("6")
-        .get("a[href='/acessos/6']")
+        .type("2")
+        .get("a[href='/acessos/2']")
         .should("be.visible")
         .wait(1000)
-        
+        .get(".flex.mr-2 > :nth-child(1) > .el-input__suffix > .el-input__suffix-inner > .el-input__icon")
+        .click();
     });
+  it("Filtro Data", () => {
+    cy
+      .licenca();
+    cy
+      .contains("li", "Acessos")
+      .click()
+      .get("span[data-v-5fa41251]")
+      .first()
+      .invoke("text")
+      .then((capturaData) => {
+        const dividiDataEmPartes = capturaData.trim().split(" ")[0];
+        const [dia, mes, ano] = dividiDataEmPartes.split("/");
+        const dataFormatada = `${ano}-${mes}-${dia}`;
+        //cy.log("Data capturada", formateDate);
+        cy
+          .get("input[placeholder=\"Início\"]")
+          .type(`${dataFormatada}{enter}`)
+          .get("input[placeholder=\"Fim\"]")
+          .type(`${dataFormatada}{enter}`)
+          .wait(1000)
+          .url()
+          .should("include", `interval=${dataFormatada}`);
+      });     
+  });  
 });
